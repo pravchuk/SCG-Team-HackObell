f5cc1705f41d787d40b494efc10fa9c5b5a370ac
ppallal <prafulla.m.pallal@gmail.com>
2014-11-09 02:57:19 +0530

    test.php

---
+++ test.php
@@ -0,0 +1,34 @@
+<?php  
+// Config /*
+extract($_GET);
+//sleep(1);
+echo '[{"id":0,"name":"',$query,'","company":"goPUC","emi":"3.14"},{"id":1,"name":"',$query,'","company":"goPUC","emi":"3.14"}]'; 
+
+
+
+//$dbhost = 'localhost';  
+//$dbname = 'test';  
+//echo "came here";
+//$username=$_POST["username"];
+//$password=$_POST["password"];
+//$url = $_POST["url"];
+// Connect to test database  
+//$m = new Mongo("mongodb://$dbhost");  
+//$db = $m->$dbname;  
+  
+// select the collection  
+
+//$collection = $db->pallal;  
+//insert a tupple
+//$collection->insert($item);
+
+// pull a cursor query  
+/*
+$aa=array('age'=>20);
+$sds=array('_id'=>false);
+$cursor = $collection->find($aa,$sds);
+  foreach($cursor as $document) 
+  {
+		echo("came here");
+  }	*/
+?>   
